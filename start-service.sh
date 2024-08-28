63b9942d930ed6b76f9c0b32d37fdfe16bfce64b
knowhw <142631271+knowhw@users.noreply.github.com>
2024-08-28 03:02:06 +0300

    Update start-service.sh

--- start-service.sh
+++ start-service.sh
@@ -1,26 +1,4 @@
 #!/bin/sh
-#sudo systemctl stop test.socket 
-
-#sudo systemctl daemon-reload
-#sudo systemctl enable --now test.socket 
-# sudo systemctl restart dc-launcher.service
-
-#sudo systemctl status test.socket
-
-#
-#sudo apt autoremove deepin-picker -y
-#sudo apt install deepin-picker -y
-
-
-# sudo systemctl stop usb-devices.service 
-
-# sudo systemctl daemon-reload
-# sudo systemctl enable --now usb-devices.service 
-# sudo systemctl restart dc-launcher.service
-
-# sudo systemctl status usb-devices.service
-
-
 sudo systemctl stop devices.service 
 # devix
 
